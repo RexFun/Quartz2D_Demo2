d75f806559b3059b4ea7da61e14fd6ebf89442f2
RexFun <oleFun@icloud.com>
2016-06-03 09:07:24 +0800

    Initial Commit

---
+++ Quartz2D_Demo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quartz2D_Demo2
+//
+//  Created by mac373 on 16/6/3.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quartz2D_Demo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quartz2D_Demo2
+//
+//  Created by mac373 on 16/6/3.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
